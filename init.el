3226f0617732e7b1d778feebb3c1890c6e0c98c6
Barry Wark <barrywark@gmail.com>
2013-03-13 12:00:30 -0400

    Sets speedbar prefs

--- init.el
+++ init.el
@@ -61,3 +61,16 @@
 
 (remove-hook 'text-mode-hook 'turn-on-auto-fill)
 (remove-hook 'text-mode-hook 'turn-on-flyspell)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(speedbar-show-unknown-files t)
+ '(speedbar-update-flag nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
